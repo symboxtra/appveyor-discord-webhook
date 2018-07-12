ec9deed816c5df93dcfbff9f06d229af86544fa9
Jack McKernan <jmcker@outlook.com>
2018-07-12 01:53:18 -0500

    Move build information to fields and improve merge commits messages
    Add version number, move OS, move build time, add build id, add co-author display, fix merge commits
    
    Closes symboxtra/jenkins-discord-webhook#2, closes symboxtra/jenkins-discord-webhook#3

--- send.ps1
+++ send.ps1
@@ -1,11 +1,12 @@
+# Modified: Symboxtra Software
 # Author: Sankarsan Kampa (a.k.a. k3rn31p4nic)
 # License: MIT
 
+$WEBHOOK_VERSION="2.0.0.0"
+
 $STATUS=$args[0]
 $WEBHOOK_URL=$args[1]
-$epoch=$(Get-Date -Date "01/01/1970")
-$date=$(Get-Date)
-$CURRENT_TIME=$([math]::Round((New-TimeSpan -Start $epoch -End $date).TotalSeconds))
+$CURRENT_TIME=[int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalSeconds
 
 if (!$WEBHOOK_URL) {
   Write-Output "WARNING!!"
@@ -44,14 +45,36 @@ $COMMIT_SUBJECT="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%s")"
 $COMMIT_MESSAGE="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%b")"
 $COMMIT_TIME="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%ct")"
 
+# Calculate approximate build time based on commit
 $BUILD_TIME=$CURRENT_TIME-$COMMIT_TIME
-$time_stamp=$([timespan]::fromseconds($BUILD_TIME))
-$DISPLAY_TIME=$("{0:mm:ss}" -f ([datetime]$time_stamp.Ticks))
+$TIME_STAMP=$([timespan]::fromseconds($BUILD_TIME))
+$DISPLAY_TIME=$("{0:mm:ss}" -f ([datetime]$TIME_STAMP.Ticks))
 
-if ($COMMIT_MESSAGE -like 'Co-author*') {
-  $COMMIT_MESSAGE=$COMMIT_MESSAGE.Split("<")
-  $COMMIT_MESSAGE=$COMMIT_MESSAGE.Split(" ")
-  $COMMIT_MESSAGE=$COMMIT_MESSAGE[1]
+# Regex match co-author names
+if ($COMMIT_MESSAGE -match 'Co-authored-by:')
+{
+    [array] $RESULTS=[regex]::Matches("$COMMIT_MESSAGE", '\w+\s\w+')
+    if ($RESULTS.Count -gt 0)
+    {
+        $CO_AUTHORS=$RESULTS.Value
+    }
+    $CO_AUTHORS = $CO_AUTHORS -join ', '
+}
+else
+{
+    $CO_AUTHORS="None"
+}
+
+# Replace git hashes in merge commits
+if ($COMMIT_SUBJECT -match 'Merge \w{40}\b into \w{40}\b')
+{
+    [array] $RESULTS=[regex]::Matches("$COMMIT_SUBJECT", '\w{40}\b')
+    foreach ($MATCH in $RESULTS)
+    {
+        $HASH=$MATCH.Value
+        $BRANCH_NAME="$(git name-rev "$HASH" --name-only)"
+        $COMMIT_SUBJECT="$COMMIT_SUBJECT" -replace "$HASH", "$BRANCH_NAME"
+    }
 }
 
 if ($AUTHOR_NAME -eq $COMMITTER_NAME) {
@@ -61,13 +84,15 @@ else {
   $CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
 }
 
+# Remove repo owner: symboxtra/project -> project
+$REPO_NAME=$env:APPVEYOR_REPO_NAME -replace '^[^/]*/', ''
+
+# Create appropriate link
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
   $URL="https://github.com/$env:APPVEYOR_REPO_NAME/pull/$env:APPVEYOR_PULL_REQUEST_NUMBER"
-  $REPO_NAME=$env:APPVEYOR_REPO_NAME
 }
 else {
   $URL="https://github.com/$env:APPVEYOR_REPO_NAME/commit/$env:APPVEYOR_REPO_COMMIT"
-  $REPO_NAME=$env:APPVEYOR_REPO_NAME -creplace '^[^/]*/', '' # Remove repo owner: symboxtra/project -> project
 }
 
 $TIMESTAMP="$(Get-Date -format s)Z"
@@ -77,14 +102,29 @@ $WEBHOOK_DATA="{
   ""embeds"": [ {
     ""color"": $EMBED_COLOR,
     ""author"": {
-      ""name"": ""#$env:APPVEYOR_BUILD_NUMBER - $REPO_NAME - Windows - $STATUS_MESSAGE"",
+      ""name"": ""#$env:APPVEYOR_BUILD_NUMBER - $REPO_NAME - $STATUS_MESSAGE"",
       ""url"": ""https://ci.appveyor.com/project/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/build/$env:APPVEYOR_BUILD_VERSION"",
       ""icon_url"": ""$AVATAR""
     },
     ""title"": ""$COMMIT_SUBJECT"",
     ""url"": ""$URL"",
-    ""description"": ""$COMMIT_MESSAGE $CREDITS"",
+    ""description"": ""$COMMIT_MESSAGE\n$CREDITS"",
     ""fields"": [
+      {
+      	""name"": ""OS"",
+      	""value"": ""Windows"",
+      	""inline"": true
+      },
+      {
+        ""name"": ""Build Time"",
+        ""value"": ""~$DISPLAY_TIME"",
+        ""inline"": true
+      },
+      {
+      	""name"": ""Build ID"",
+      	""value"": ""${env:APPVEYOR_BUILD_NUMBER}CI"",
+      	""inline"": true
+      },
       {
         ""name"": ""Commit"",
         ""value"": ""[``$($env:APPVEYOR_REPO_COMMIT.substring(0, 7))``](https://github.com/$env:APPVEYOR_REPO_NAME/commit/$env:APPVEYOR_REPO_COMMIT)"",
@@ -94,9 +134,17 @@ $WEBHOOK_DATA="{
         ""name"": ""Branch/Tag"",
         ""value"": ""[``$env:APPVEYOR_REPO_BRANCH``](https://github.com/$env:APPVEYOR_REPO_NAME/tree/$env:APPVEYOR_REPO_BRANCH)"",
         ""inline"": true
+      },
+      {
+        ""name"": ""Co-Authors"",
+        ""value"": ""$CO_AUTHORS"",
+        ""inline"": true
       }
     ],
-    ""timestamp"": ""$TIMESTAMP ($DISPLAY_TIME)""
+    ""footer"": {
+        ""text"": ""v$WEBHOOK_VERSION""
+      },
+    ""timestamp"": ""$TIMESTAMP""
   } ]
 }"
 
